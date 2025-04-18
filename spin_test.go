a78d532fbd81397171fb80f3dae84cf7257ba5b2
Tom Fleet <tomfleet2018@gmail.com>
2025-04-18 10:20:56 +0100

    Add tests for double start stop (#3)

--- spin_test.go
+++ spin_test.go
@@ -6,24 +6,75 @@ import (
 	"testing"
 	"time"
 
+	"github.com/FollowTheProcess/hue"
 	"github.com/FollowTheProcess/spin"
 )
+
+var frames = [...]string{"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"}
 
 func TestSpinner(t *testing.T) {
 	buf := &bytes.Buffer{}
 
-	spinner := spin.New(buf, "Testing")
+	spinner := spin.New(buf, "Testing", spin.FrameStyle(hue.Yellow), spin.MessageStyle(hue.Blue))
 	spinner.Start()
 	time.Sleep(300 * time.Millisecond)
 	spinner.Stop()
 
 	got := buf.String()
 
-	frames := [...]rune{'⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'}
+	found := false
+	for _, frame := range frames {
+		if strings.Contains(got, frame) {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		t.Fatalf("expected output to contain a spinner frame:\n\n%s\n", got)
+	}
+}
+
+func TestDoubleStart(t *testing.T) {
+	buf := &bytes.Buffer{}
+
+	spinner := spin.New(buf, "Working")
+	spinner.Start()
+	spinner.Start() // This one shouldn't do anything
+
+	time.Sleep(300 * time.Millisecond)
+	spinner.Stop()
+
+	got := buf.String()
 
 	found := false
 	for _, frame := range frames {
-		if strings.Contains(got, string(frame)) {
+		if strings.Contains(got, frame) {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		t.Fatalf("expected output to contain a spinner frame:\n\n%s\n", got)
+	}
+}
+
+func TestDoubleStop(t *testing.T) {
+	buf := &bytes.Buffer{}
+
+	spinner := spin.New(buf, "Working")
+	spinner.Start()
+
+	time.Sleep(300 * time.Millisecond)
+	spinner.Stop()
+	spinner.Stop() // This one shouldn't do anything
+
+	got := buf.String()
+
+	found := false
+	for _, frame := range frames {
+		if strings.Contains(got, frame) {
 			found = true
 			break
 		}
